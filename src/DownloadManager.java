72bd6f1070302067fa8ff61b11b57a6d6197ab4a
n0Live <egor.krasnopolin@gmail.com>
2015-02-14 10:16:47 +0600

    Returned runtime errors handling

--- src/DownloadManager.java
+++ src/DownloadManager.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Locale;
@@ -84,17 +85,22 @@ public class DownloadManager {
 			try (BufferedReader reader = new BufferedReader(new InputStreamReader(in, "Cp1251"))) {
 				String line;
 				while ((line = reader.readLine()) != null) {
-					String[] values = line.split(" ");
-					String URL = values[0];
-					String fileName = values[1];
-					
-					Set<String> filesSet = result.get(URL);
-					if (filesSet == null) {
-						filesSet = new HashSet<>();
+					try {
+						String[] values = line.split(" ");
+						String URL = values[0];
+						String fileName = values[1];
+						
+						Set<String> filesSet = result.get(URL);
+						if (filesSet == null) {
+							filesSet = new HashSet<>();
+						}
+						// добавляем к имени файла путь к папке для сохранения
+						filesSet.add(outputFolder + File.separator + fileName);
+						result.put(URL, filesSet);
+					} catch (Exception e) {
+						System.err.println("ОШИБКА: ошибка разбора строки " + line + "\n"
+						        + e.toString());
 					}
-					// добавляем к имени файла путь к папке для сохранения
-					filesSet.add(outputFolder + File.separator + fileName);
-					result.put(URL, filesSet);
 				}
 			}
 		} catch (IOException e) {
@@ -142,11 +148,11 @@ public class DownloadManager {
 			String avgSpeed = String.format(Locale.ENGLISH, "%.2f байт/c", downloadedBytes
 			        / ((float) elapsedTime / 1000));
 			
+			System.err.flush(); // сначала выводим все ошибки
 			System.out.println("Скачано байт: " + sDownloadedBytes);
 			System.out.println("Время работы: " + workedTime);
-			System.out.print("Средняя скорость: " + avgSpeed);
-			System.out.println(speedLimit > 0 ? " (Ограничение: " + speedLimit + " байт/c)" : "");
-			
+			System.out.println("Средняя скорость: " + avgSpeed
+			        + (speedLimit > 0 ? " (Ограничение: " + speedLimit + " байт/c)" : ""));
 		} catch (Exception e) {
 			System.err.println("ОШИБКА: " + e.getLocalizedMessage());
 			errorsCount++;
@@ -162,6 +168,7 @@ public class DownloadManager {
 	 * Запуск закачек
 	 */
 	public void startWork() {
+		ArrayList<Future<?>> futures = new ArrayList<>();
 		int errorsCount = 0; // счетчик ошибок
 		
 		// менеджер ограничения скорости закачек
@@ -171,13 +178,25 @@ public class DownloadManager {
 		
 		for (String url : urls.keySet()) {
 			try {
-				downloadPool.execute(new Downloader(url, urls.get(url), speedManager));
+				futures.add(downloadPool.submit(new Downloader(url, urls.get(url), speedManager)));
 			} catch (MalformedURLException e) {
 				System.err.println("ОШИБКА: " + e.getLocalizedMessage());
 				errorsCount++;
 			}
 		}
 		downloadPool.shutdown();
+		
+		// обработка ошибок
+		for (Future<?> future2 : futures) {
+			try {
+				future2.get();
+			} catch (ExecutionException e) {
+				System.err.println("ОШИБКА: " + e.getLocalizedMessage());
+				errorsCount++;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		
 		try {
 			downloadPool.awaitTermination(Integer.MAX_VALUE, TimeUnit.MILLISECONDS);
